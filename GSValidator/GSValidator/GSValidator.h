3f3921f0e4c6b0aba11dda96d435f3a5f47c19e7
Govind Solanki <govind.solanki@appinventiv.com>
2022-08-18 18:21:30 +0530

    Adding ValidatorSDK

---
+++ GSValidator/GSValidator/GSValidator.h
@@ -0,0 +1,18 @@
+//
+//  GSValidator.h
+//  GSValidator
+//
+//  Created by Govind Solanki on 2022-08-18.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GSValidator.
+FOUNDATION_EXPORT double GSValidatorVersionNumber;
+
+//! Project version string for GSValidator.
+FOUNDATION_EXPORT const unsigned char GSValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GSValidator/PublicHeader.h>
+
+
